d0ceaaa99be08f2f74c096eb37317bbb4f3848f3
BasovMaxym <maksimka.basov2000@gmail.com>
2020-05-14 19:01:33 +0300

    Initial commit

---
+++ src/contollernews/ControllerNews.java
@@ -0,0 +1,70 @@
+package contollernews;
+
+import news.News;
+
+import java.text.DateFormat;
+
+import java.text.SimpleDateFormat;
+
+
+
+public class ControllerNews
+{
+
+    public ControllerNews(News[] arrayNews)
+    {
+
+        this.arrayNews = arrayNews;
+
+    }
+
+    public void view(int id)
+    {
+
+        try
+        {
+
+            DateFormat dateFormat = new SimpleDateFormat("dd.M.yyyy HH:mm:ss:SSS");
+
+            System.out.println("Назва: " + arrayNews[id].getNews() + "\n" + "Дата створення: " + dateFormat.format(arrayNews[id].getDateCreation().getTime()) + "\n" + "Дата зміни: " + dateFormat.format(arrayNews[id].getDateChange().getTime()));
+
+            System.out.println();
+
+        }
+        catch (ArrayIndexOutOfBoundsException ex)
+        {
+
+            System.out.println("Error:");
+
+            ex.printStackTrace();
+
+        }
+
+    }
+
+    public void edit(int id,String title)
+    {
+
+        try
+        {
+
+            arrayNews[id].changeNews(title);
+
+            this.view(id);
+
+        }
+        catch(ArrayIndexOutOfBoundsException ex)
+        {
+
+            System.out.println("Error:");
+
+            ex.printStackTrace();
+
+        }
+
+    }
+
+    private News[] arrayNews;
+
+
+}
